80057d956d682ee4df23ad38d928f754df93c83c
Mamoon Idrees <mamoon.idrees@gmail.com>
2026-01-14 01:37:58 +0530

    testing92

--- Downloads/demo-new-fixed/infra/terraform/dev/backend.tf
+++ Downloads/demo-new-fixed/infra/terraform/dev/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "vursa-terraform-state-prod"
-    key    = "dev/terraform.tfstate"
-    region = "ap-south-1"
-
+    bucket       = "vursa-terraform-state-prod"
+    key          = "dev/terraform.tfstate"
+    region       = "ap-south-1"
     use_lockfile = true
   }
 }
